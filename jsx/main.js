ee11a1f674425ac6925218030e5e06f9eb5de796
sevenis7 <aleeyyu+01@gmail.com>
2015-08-06 20:38:38 +0900

    add +1/-1 emoji

--- jsx/main.js
+++ jsx/main.js
@@ -11,8 +11,14 @@ var Food = React.createClass({
           </div>
           <div className="row">
             <div className="col-md-6 col-xs-6 col-sm-6">
-              <img src="https://a248.e.akamai.net/assets.github.com/images/icons/emoji/moneybag.png" alt="Credits" style={{height: '16px', width: '16px'}} data-toggle="tooltip" title="" data-original-title="Credits" />
-              109
+              <a href="http://lgtm.in/u/GdELVjxmq" data-toggle="tooltip" title="" data-original-title="Upvote">
+                <img src="https://a248.e.akamai.net/assets.github.com/images/icons/emoji/+1.png" alt="Like" style={{height: '16px', width: '16px'}} />
+              </a>
+            </div>
+            <div className="col-md-6 col-xs-6 col-sm-6" style={{'text-align': 'right'}}>
+              <a href="http://lgtm.in/r/GdELVjxmq" data-toggle="tooltip" title="" data-original-title="Report">
+                <img src="https://a248.e.akamai.net/assets.github.com/images/icons/emoji/-1.png" alt="Report" style={{height: '16px', width: '16px'}} />
+              </a>
             </div>
           </div>
         </div>
